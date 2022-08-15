bb536d16f02c6633fd2a4b63462b3cefca15595a
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-08-15 08:55:14 +0000

    [応用課題６]いいね機能　非同期通信

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: 'popper.js'
+    jQuery: 'jquery/src/jquery'
   })
 )
 
